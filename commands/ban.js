39e4b0293d2c2f393d52d2c604e4e0c4951d6431
Bysimeit <casanosimon@gmail.com>
2022-11-30 21:51:26 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -22,24 +22,22 @@ module.exports = {
 
     async run(bot, message, args) {
         try {
-            let user = await bot.user.fetch(args._hoistedOptions[0].value);
+            let user = await bot.users.fetch(args._hoistedOptions[0].value);
             if (!user) return message.reply("Pas de membre à bannir !");
             let member = message.guild.members.cache.get(user.id);
 
-            let reason = args.get("raison").value;
+            let reason = args.getString("raison");
             if (!reason) reason = "Pas de raison fournie.";
 
             if (message.user.id === user.id) return message.reply("Tu ne peux pas te bannir ;)");
             if ((await message.guild.fetchOwner()).id === user.id) return message.reply("Tu ne peux pas bannir le propriétaire du serveur !");
-            if (member && !member?.bannable) return message.reply("Je ne peux pas bannir ce membre ! :/");
+            if (member && !member.bannable) return message.reply("Je ne peux pas bannir ce membre ! :/");
             if (member && message.member.roles.highest.comparePostionTo(member.roles.highest) <= 0) return message.reply("Tu ne peux pas bannir un membre qui à un rôle supérieur ou égal à toi !");
             if ((await message.guild.bans.fetch()).get(user.id)) return message.reply("Ce membre est déjà banni.");
 
             try {
                 await user.send(`Tu as été banni du serveur ${message.guild.name} par ${message.user.tag} pour la raison : \`${reason}\``)
-            } catch (e) {
-
-            }
+            } catch (e) {}
 
             await message.reply(`${message.user} a banni ${user.tag} pour la raison : \`${reason}\``);
             await message.guild.bans.create(user.id, {reason: reason});
